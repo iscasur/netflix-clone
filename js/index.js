7ed7b115f428ddbd0ba81afbeb4e12eb98e05442
Israel Castro <israel.castro@gmail.com>
2021-04-28 19:03:10 -0500

    [Add] search-box

--- js/index.js
+++ js/index.js
@@ -10,3 +10,12 @@ window.addEventListener('scroll', function () {
     navbar.classList.remove('dark');
   }
 });
+
+const searchBox = document.getElementsByClassName('search-box')[0];
+const searchIcon = document.getElementsByClassName('search-box__icon')[0];
+
+searchIcon.addEventListener('click', activateSearch);
+
+function activateSearch() {
+  searchBox.classList.toggle('active');
+}
