a55ceb478d4771e2f6cacb585c70129cf10bcd74
Israel Castro <israel.castro@gmail.com>
2021-04-28 20:26:23 -0500

    [Add] carousel

--- js/index.js
+++ js/index.js
@@ -11,11 +11,37 @@ window.addEventListener('scroll', function () {
   }
 });
 
-const searchBox = document.getElementsByClassName('search-box')[0];
-const searchIcon = document.getElementsByClassName('search-box__icon')[0];
+const searchBox = document.querySelector('.search-box');
+const searchIcon = document.querySelector('.search-box__icon');
 
 searchIcon.addEventListener('click', activateSearch);
 
 function activateSearch() {
   searchBox.classList.toggle('active');
+}
+
+const rows = document.querySelector('.carrousel-content');
+const movies = document.querySelectorAll('.carrousel__item');
+
+const leftArrow = document.getElementById('left-arrow');
+const rigthArrow = document.getElementById('right-arrow');
+
+leftArrow.addEventListener('click', () => {
+  rows.scrollLeft -= rows.offsetWidth;
+});
+
+rigthArrow.addEventListener('click', () => {
+  rows.scrollLeft += rows.offsetWidth;
+});
+
+const accessKey = aadSocsmWTjPn6vTCVMHHE5KNYptnq0RwPqhlTFols8;
+const endPoint = 'https://api.unsplash.com/search/photos';
+
+async function getImages(query) {
+  let response = await fetch(
+    endPoint + '?query=' + query + '&client_id=' + accessKey
+  );
+  let jsonResponse = await response.json();
+  let imagesList = await jsonResponse.results;
+  console.log(imagesList);
 }
